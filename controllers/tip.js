8fc6adab0323dc88ca323b5af22b95f6ce548a7e
Marta de Miguel <m.demiguelr@alumnos.upm.es>
2018-06-11 10:10:49 +0200

    version14

--- controllers/tip.js
+++ controllers/tip.js
@@ -59,14 +59,15 @@ exports.update = (req, res, next) => {
 
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
-
+    const authorId = req.session.user && req.session.user.id || 0;
     const tip = models.tip.build(
         {
             text: req.body.text,
-            quizId: req.quiz.id
+            quizId: req.quiz.id,
+            authorId
         });
 
-    tip.save()
+    tip.save({fields: ["text","quizId","authorId"]})
         .then(tip => {
             req.flash('success', 'Tip created successfully.');
             res.redirect("back");
